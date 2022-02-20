b082948999aa49a9d3e1d888d1d7656779775a8b
Usama Tufail <miansamusama@gmail.com>
2022-02-20 14:30:16 +0500

    fix(homepage): fixed responsiveness for homepage

--- src/web/pages/order-page/sections/Done.section.jsx
+++ src/web/pages/order-page/sections/Done.section.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { withNamespaces } from 'react-i18next';
 import { Field } from 'redux-form';
 import { useNavigate } from 'react-router-dom';
 
