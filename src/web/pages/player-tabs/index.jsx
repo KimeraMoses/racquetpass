549a3b6ace68836f38a3daa73ca2b1d18dca5d2a
Usama <miansamusama@gmail.com>
2022-02-22 17:37:41 +0500

    feat(cover-7): first screen design completed for cover 7

--- src/web/pages/player-tabs/index.jsx
+++ src/web/pages/player-tabs/index.jsx
@@ -2,22 +2,17 @@ import { useState } from 'react';
 import { reduxForm } from 'redux-form';
 import { useNavigate } from 'react-router-dom';
 import { withNamespaces } from 'react-i18next';
-import {
-  AccountButton,
-  SubHeading,
-  BackButton,
-  Heading,
-  CustomInput,
-  SubmitButton,
-} from 'web/components';
 import './player-tabs.styles.scss';
+import { Locker } from './sections';
 
 function PlayerTabs({ t, handleSubmit }) {
   const navigate = useNavigate();
   return (
     <>
       <form onSubmit={handleSubmit} className="player-tabs">
-        <div>Player Tabs</div>
+        <div>
+          <Locker t={t} />
+        </div>
       </form>
     </>
   );
